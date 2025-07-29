e3c33043ba2847b99351dfaeef3c9d189d7f2f61
iyenagbe <iyenagberaymond@gmail.com>
2025-07-29 23:51:48 +0100

    make changes

--- frontend/frontend/src/Components/Forms.jsx
+++ frontend/frontend/src/Components/Forms.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 
 const Forms = () => {
   return (
-    <form className='border ' >
+    <div className='text-center mt-10'>
+    <form className='' >
     <input className='' type="text" name="name" placeholder="Full Name" required />
     <input className='' type="email" name="email" placeholder="Email" required />
     <input className='' type="password" name="password" placeholder="Password" required />
     <button className='' type="submit">Register</button>
     </form>
+    </div>
 
   )
 }
