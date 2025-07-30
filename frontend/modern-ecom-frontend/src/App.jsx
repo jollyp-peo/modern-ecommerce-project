fa39b99295472d26b6dfa0f43ff93b819a0dcd11
iyenagbe <iyenagberaymond@gmail.com>
2025-07-30 12:36:22 +0100

    frontend

---
+++ frontend/modern-ecom-frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Forms from './Componets/Forms'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Forms />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
