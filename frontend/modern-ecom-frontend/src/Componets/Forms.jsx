af2d319b83d5f8910b0b8527a01c840321e17515
iyenagbe <iyenagberaymond@gmail.com>
2025-07-30 20:28:34 +0100

    make changes

--- frontend/modern-ecom-frontend/src/Componets/Forms.jsx
+++ frontend/modern-ecom-frontend/src/Componets/Forms.jsx
@@ -2,9 +2,52 @@ import React from 'react'
 
 const Forms = () => {
   return (
-    <div>
-      
-    </div>
+  <div className="flex justify-center items-center h-screen bg-green-100">
+    <form className="bg-white p-8 rounded shadow-md w-full max-w-md space-y-4">
+    <input type="text" name="Name" placeholder="Full Name" required className="px-4 py-2 border border-green-300 rounded-md"
+    />
+      <input type="email" name="Email" placeholder="Email" required className="w-full sm:flex-1 outline-none py-4"
+    />
+      <input type="tel" name="phone" placeholder="Phone" required className="px-4 py-2 border border-green-300 rounded-md"
+    />
+    <input type="submit" value="Submit"
+      className="bg-green-700 hover:bg-green-500 active:bg-green-900 rounded text-white text-xs px-10 py-6" />
+    </form>
+  </div>
+
+//   <div className="flex justify-center items-center h-screen bg-green-100">
+//   <form className="bg-white p-8 rounded shadow-md w-full max-w-4xl flex flex-wrap gap-4 justify-between items-center">
+//     <input
+//       type="text"
+//       name="Name"
+//       placeholder="Full Name"
+//       required
+//       className="flex-1 min-w-[200px] px-4 py-2 border border-green-300 rounded-md"
+//     />
+//     <input
+//       type="email"
+//       name="Email"
+//       placeholder="Email"
+//       required
+//       className="flex-1 min-w-[200px] px-4 py-2 border border-green-300 rounded-md"
+//     />
+//     <input
+//       type="tel"
+//       name="phone"
+//       placeholder="Phone"
+//       required
+//       className="flex-1 min-w-[200px] px-4 py-2 border border-green-300 rounded-md"
+//     />
+//     <input
+//       type="submit"
+//       value="Submit"
+//       className="bg-green-700 hover:bg-green-500 active:bg-green-900 rounded text-white text-sm px-8 py-3"
+//     />
+//   </form>
+// </div>
+
+
+
   )
 }
 
